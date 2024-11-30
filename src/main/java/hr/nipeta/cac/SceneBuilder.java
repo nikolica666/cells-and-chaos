3646da1de877846e5399a4b207df7052c7c90cd3
nikolica666 <nikola.petanjak@gmail.com>
2024-12-01 00:49:23 +0100

    Cosmetics

--- src/main/java/hr/nipeta/cac/SceneBuilder.java
+++ src/main/java/hr/nipeta/cac/SceneBuilder.java
@@ -1,19 +1,11 @@
 package hr.nipeta.cac;
 
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.geometry.Rectangle2D;
 import javafx.scene.Node;
 import javafx.scene.Scene;
-import javafx.scene.control.Alert;
-import javafx.scene.control.Button;
-import javafx.scene.control.TextInputControl;
-import javafx.scene.control.Tooltip;
-import javafx.scene.input.KeyCode;
 import javafx.scene.layout.HBox;
 import javafx.stage.Screen;
 import javafx.stage.Stage;
-import javafx.util.Duration;
 
 import java.util.function.Supplier;
 
@@ -46,17 +38,6 @@ public abstract class SceneBuilder {
         Rectangle2D screenBounds = Screen.getPrimary().getVisualBounds();
         stage.setX(screenBounds.getMinX() + (screenBounds.getWidth() - stage.getScene().getWidth()) / 2);
         stage.setY(screenBounds.getMinY() + (screenBounds.getHeight() - stage.getScene().getHeight()) / 2);
-    }
-
-    protected TextInputControl onTextInputEnter(TextInputControl input, Runnable action) {
-        input.setOnKeyPressed(event -> {
-            if (event.getCode() == KeyCode.ENTER) {
-                if (!input.getText().isEmpty()) {
-                    action.run();
-                }
-            }
-        });
-        return input;
     }
 
     protected HBox horizontalMenu(Node... nodes) {
